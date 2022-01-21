9424d03daccf94ae337b2be2e13010a019f66323
kgmsdg <89608873+kgmsdg@users.noreply.github.com>
2022-01-21 17:04:59 +0300

    first commit

---
+++ templates/work/Menu2.js
@@ -0,0 +1,180 @@
+
+const Menu3 = () => {
+
+    return (
+
+
+        /*#__PURE__*/
+        React.createElement("div", {
+            className: "hor-menu "
+        }, /*#__PURE__*/React.createElement("ul", {
+            className: "nav navbar-nav"
+        }, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
+            href: "index.html"
+        }, "Dashboard")), /*#__PURE__*/React.createElement("li", {
+            className: "menu-dropdown classic-menu-dropdown "
+        }, /*#__PURE__*/React.createElement("a", {
+            "data-hover": "megamenu-dropdown",
+            "data-close-others": "true",
+            "data-toggle": "dropdown",
+            href: "javascript:;"
+        }, "Proje Y\xF6netimi ", /*#__PURE__*/React.createElement("i", {
+            className: "fa fa-angle-down"
+        })), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu pull-left"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " dropdown-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: ":;"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-briefcase"
+        }), "Proje "), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "form_Proje.html"
+        }, "Yeni Giri\u015F ")), /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_Proje.html"
+        }, "Liste ")))), /*#__PURE__*/React.createElement("li", {
+            className: " dropdown-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: ":;"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-briefcase"
+        }), "Daire "), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "form_Daire.html"
+        }, "Yeni Giri\u015F ")), /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_Daire.html"
+        }, "Liste ")))), /*#__PURE__*/React.createElement("li", {
+            className: " dropdown-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: ":;"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-briefcase"
+        }), "\xC7al\u0131\u015Fan "), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "form_Calisan.html"
+        }, "Yeni Giri\u015F ")), /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_Calisan.html"
+        }, "Liste ")))))), /*#__PURE__*/React.createElement("li", {
+            className: "menu-dropdown classic-menu-dropdown "
+        }, /*#__PURE__*/React.createElement("a", {
+            "data-hover": "megamenu-dropdown",
+            "data-close-others": "true",
+            "data-toggle": "dropdown",
+            href: "javascript:;"
+        }, "M\xFC\u015Fteri Y\xF6netimi ", /*#__PURE__*/React.createElement("i", {
+            className: "fa fa-angle-down"
+        })), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu pull-left"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " dropdown-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: ":;"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-briefcase"
+        }), "M\xFC\u015Fteri "), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "form_musteri.html"
+        }, "Yeni Giri\u015F ")), /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_musteri.html"
+        }, "Liste ")))), /*#__PURE__*/React.createElement("li", {
+            className: " dropdown-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: ":;"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-eye"
+        }), "Ziyaret "), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "form_ziyaret.html"
+        }, "Yeni Giri\u015F ")), /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_ziyaret.html"
+        }, "Liste ")))), /*#__PURE__*/React.createElement("li", {
+            className: " dropdown-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: ":;"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-bar-chart"
+        }), "Sat\u0131\u015F "), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "form_satis.html"
+        }, "Yeni Giri\u015F ")), /*#__PURE__*/React.createElement("li", {
+            className: " "
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_satis.html"
+        }, "Liste ")))))), /*#__PURE__*/React.createElement("li", {
+            className: "menu-dropdown classic-menu-dropdown "
+        }, /*#__PURE__*/React.createElement("a", {
+            "data-hover": "megamenu-dropdown",
+            "data-close-others": "true",
+            "data-toggle": "dropdown",
+            href: "javascript:;"
+        }, "\u0130\xE7erik Y\xF6netimi ", /*#__PURE__*/React.createElement("i", {
+            className: "fa fa-angle-down"
+        })), /*#__PURE__*/React.createElement("ul", {
+            className: "dropdown-menu pull-left"
+        }, /*#__PURE__*/React.createElement("li", {
+            className: " mega-menu-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_Cinsiyet.html"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-settings"
+        }), "Cinsiyet ")), /*#__PURE__*/React.createElement("li", {
+            className: " mega-menu-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_DaireTipi.html"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-settings"
+        }), "Daire Tipi ")), /*#__PURE__*/React.createElement("li", {
+            className: " mega-menu-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_GelirTipi.html"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-settings"
+        }), "Gelir Tipi ")), /*#__PURE__*/React.createElement("li", {
+            className: " mega-menu-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_ProjeDurumu.html"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-settings"
+        }), "Proje Durumu ")), /*#__PURE__*/React.createElement("li", {
+            className: " mega-menu-submenu"
+        }, /*#__PURE__*/React.createElement("a", {
+            href: "liste_Sehir.html"
+        }, /*#__PURE__*/React.createElement("i", {
+            className: "icon-settings"
+        }), "\u015Eehir "))))))
+    )
+    
+
+}
+const domContainer = document.querySelector('#reactMenu');
+
+ReactDOM.render(React.createElement(Menu3), domContainer);
